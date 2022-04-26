6cb27a3711eeb17738bc5bd4a6d5accb3b9a5b2d
Nour Azouz <nourazouz7@gmail.com>
2022-04-26 11:16:47 +0000

    feat : init orderdetails crud [sc-52]

--- apps/api/src/app/order-details/dto/update-order-detail.input.ts
+++ apps/api/src/app/order-details/dto/update-order-detail.input.ts
@@ -1,8 +1,7 @@
-import { CreateOrderDetailInput } from './create-order-detail.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { CreateOrderDetailInput } from "./create-order-detail.input";
+import { InputType, Field, ID, PartialType } from "@nestjs/graphql";
 
 @InputType()
-export class UpdateOrderDetailInput extends PartialType(CreateOrderDetailInput) {
-  @Field(() => Int)
-  id: number;
-}
+export class UpdateOrderDetailInput extends PartialType(
+  CreateOrderDetailInput
+) {}
