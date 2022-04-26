b02c29951987310285bb55fada2c54e875c0186b
Nour Azouz <nourazouz7@gmail.com>
2022-04-26 11:18:32 +0000

    Feat : init branch crud [sc-49]

--- apps/api/src/app/branches/branches.resolver.ts
+++ apps/api/src/app/branches/branches.resolver.ts
@@ -1,35 +1,40 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { BranchesService } from './branches.service';
-import { Branch } from './entities/branch.entity';
-import { CreateBranchInput } from './dto/create-branch.input';
-import { UpdateBranchInput } from './dto/update-branch.input';
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
+import { BranchesService } from "./branches.service";
+import { Branch } from "./entities/branch.entity";
+import { CreateBranchInput } from "./dto/create-branch.input";
+import { UpdateBranchInput } from "./dto/update-branch.input";
 
 @Resolver(() => Branch)
 export class BranchesResolver {
   constructor(private readonly branchesService: BranchesService) {}
 
   @Mutation(() => Branch)
-  createBranch(@Args('createBranchInput') createBranchInput: CreateBranchInput) {
+  createBranch(
+    @Args("createBranchInput") createBranchInput: CreateBranchInput
+  ) {
     return this.branchesService.create(createBranchInput);
   }
 
-  @Query(() => [Branch], { name: 'branches' })
+  @Query(() => [Branch], { name: "branches" })
   findAll() {
     return this.branchesService.findAll();
   }
 
-  @Query(() => Branch, { name: 'branch' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Branch, { name: "branch" })
+  findOne(@Args("id", { type: () => String }) id: string) {
     return this.branchesService.findOne(id);
   }
 
   @Mutation(() => Branch)
-  updateBranch(@Args('updateBranchInput') updateBranchInput: UpdateBranchInput) {
-    return this.branchesService.update(updateBranchInput.id, updateBranchInput);
+  updateBranch(
+    @Args("updateBranchInput") updateBranchInput: UpdateBranchInput,
+    @Args("id", { type: () => String }) id: string
+  ) {
+    return this.branchesService.update(id, updateBranchInput);
   }
 
   @Mutation(() => Branch)
-  removeBranch(@Args('id', { type: () => Int }) id: number) {
+  removeBranch(@Args("id", { type: () => String }) id: string) {
     return this.branchesService.remove(id);
   }
 }
